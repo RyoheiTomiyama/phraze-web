9ddab682eefeaf237cf5b6a7b8fdf56f362d487d
RyoheiTomiyama <tennisnowboarderyou@gmail.com>
2024-06-18 23:03:51 +0900

    dashboardページにもレイアウト当てる

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,17 +1,10 @@
-import { LayoutWithSidebar } from '@/components/common/layout'
-import { SideNav } from '@/components/common/layout/sidebar/side-nav'
 import { DeckTable } from '@/components/feature/deck'
-import { Heading } from '@/components/ui/heading'
+import { DefaultLayout } from '@/components/feature/layout'
 
 export default function DashboardPage() {
   return (
-    <LayoutWithSidebar sidebar={<SideNav />}>
-      <header className="container">
-        <Heading variant="h1">Dashboard</Heading>
-      </header>
-      <main className="container">
-        <DeckTable />
-      </main>
-    </LayoutWithSidebar>
+    <DefaultLayout title="Dashboard">
+      <DeckTable />
+    </DefaultLayout>
   )
 }
