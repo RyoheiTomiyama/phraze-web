1baf43c6d83099cf20f2658f25aaeaa124d2c0a2
RyoheiTomiyama <tennisnowboarderyou@gmail.com>
2024-07-07 23:02:22 +0900

    gql 叩く

--- src/components/feature/deck/deck-table.tsx
+++ src/components/feature/deck/deck-table.tsx
@@ -6,11 +6,17 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
-import { ArrowRight, Plus } from 'lucide-react'
+import { ArrowRight } from 'lucide-react'
 import Link from 'next/link'
 import { CreateDeckButton } from './create-deck'
+import { DeckOnDeckTableFragment } from './deck-table.generated'
+import { cn } from '@/lib/utils'
+import { pagesPath } from '@/lib/pathpida/$path'
 
-export const DeckTable = () => {
+type DeckTableProps = {
+  decks: DeckOnDeckTableFragment[]
+}
+export const DeckTable = ({ decks }: DeckTableProps) => {
   return (
     <Card x-chunk="dashboard-06-chunk-0">
       <CardHeader className="flex flex-row items-center">
@@ -23,37 +29,35 @@ export const DeckTable = () => {
         <CreateDeckButton />
       </CardHeader>
       <CardContent className="flex flex-col gap-8">
-        <div className="flex items-center gap-4">
-          <div className="grid gap-1 flex-1">
-            <p className="text-sm font-medium leading-none">Olivia Martin</p>
-            <p className="text-sm text-muted-foreground break-all">
-              olivia.martin@email.com
-            </p>
-          </div>
-          <div className="">
-            <Button className="gap-2" asChild>
-              <Link href="/deck/1">
-                学習する
-                <ArrowRight className="w-4" />
-              </Link>
-            </Button>
-          </div>
-        </div>
-        <div className="flex items-center gap-4">
-          <div className="grid gap-1 flex-1">
-            <p className="text-sm font-medium leading-none">Olivia Martin</p>
-            <p className="text-sm text-muted-foreground break-all">
-              olivia.martin@email.com
-            </p>
-          </div>
-          <div className="">
-            <Button className="gap-2">
-              学習する
-              <ArrowRight className="w-4" />
-            </Button>
-          </div>
-        </div>
+        {decks.map((d) => {
+          return <CardItem key={d.id} deck={d} />
+        })}
       </CardContent>
     </Card>
+  )
+}
+
+const CardItem = ({
+  deck,
+  className,
+}: {
+  deck: DeckOnDeckTableFragment
+  className?: string
+}) => {
+  return (
+    <div className={cn('flex items-center gap-4', className)}>
+      <div className="grid gap-1 flex-1">
+        <p className="text-sm font-medium leading-none">Olivia Martin</p>
+        <p className="text-sm text-muted-foreground break-all">{deck.name}</p>
+      </div>
+      <div className="">
+        <Button className="gap-2" asChild>
+          <Link href={pagesPath.deck._id(deck.id).$url()}>
+            学習する
+            <ArrowRight className="w-4" />
+          </Link>
+        </Button>
+      </div>
+    </div>
   )
 }
---
+++ src/components/template/dashboard/dashboard.tsx
@@ -0,0 +1,12 @@
+import { DeckTable } from '@/components/feature/deck'
+import { DefaultLayout } from '@/components/feature/layout'
+import { useDecksOnDashboardQuery } from './dashboard.generated'
+
+export const Dashboard = () => {
+  const [{ data, fetching, error }] = useDecksOnDashboardQuery()
+  return (
+    <DefaultLayout title="Dashboard">
+      <DeckTable decks={data?.decks || []} />
+    </DefaultLayout>
+  )
+}
