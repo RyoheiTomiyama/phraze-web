81d4ff2a4b347a4fb05b003e2f4d65eef4b2f346
RyoheiTomiyama <tennisnowboarderyou@gmail.com>
2024-07-11 23:24:36 +0900

    feat: toaster

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { DialogProvider } from '@/components/common/dialog'
 import { AuthProvider } from '@/components/feature/auth'
 import { GraphProvider } from '@/components/feature/graph'
+import { Toaster } from '@/components/ui/sonner'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
@@ -11,6 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <DialogProvider>
           <Component {...pageProps} />
         </DialogProvider>
+        <Toaster />
       </GraphProvider>
     </AuthProvider>
   )
