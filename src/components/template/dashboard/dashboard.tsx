706198bf856a5650af48d8e238810daa0d465c83
RyoheiTomiyama <tennisnowboarderyou@gmail.com>
2024-08-03 22:09:14 +0900

    chore: ダッシュボードのキャッシュしないように

--- src/components/template/dashboard/dashboard.tsx
+++ src/components/template/dashboard/dashboard.tsx
@@ -3,7 +3,9 @@ import { useDecksOnDashboardQuery } from './dashboard.generated'
 import { DeckList } from '@/components/feature/deck/deck-list'
 
 export const Dashboard = () => {
-  const [{ data, fetching, error }] = useDecksOnDashboardQuery()
+  const [{ data, fetching, error }] = useDecksOnDashboardQuery({
+    requestPolicy: 'cache-and-network',
+  })
   return (
     <DefaultLayout title="Dashboard">
       <DeckList decks={data?.decks.decks || []} />
