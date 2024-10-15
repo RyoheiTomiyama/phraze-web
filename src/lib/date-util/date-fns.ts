bdb7045f86ea042b62b20eddd2c7adf4977b8afd
RyoheiTomiyama <tennisnowboarderyou@gmail.com>
2024-10-15 23:29:36 +0900

    feat: date-util.format

--- src/lib/date-util/date-fns.ts
+++ src/lib/date-util/date-fns.ts
@@ -5,6 +5,9 @@ export const add = (d: Date, duration: Duration) => {
   return datefns.add(d, duration)
 }
 
+export const formatDateTime = (d: Date | string) => {
+  return datefns.format(d, 'yyyy-MM-dd HH:mm')
+}
 export const formatDistanceToNowStrict = (d: Date | string) => {
   return datefns.formatDistanceToNowStrict(d, { addSuffix: true })
 }
