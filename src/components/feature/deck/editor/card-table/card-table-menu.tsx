7b41eed0cb4f38f8715975ae8acd6a2a9dcce3b7
RyoheiTomiyama <tennisnowboarderyou@gmail.com>
2024-11-11 22:39:58 +0900

    feat: カード削除in table

--- src/components/feature/deck/editor/card-table/card-table-menu.tsx
+++ src/components/feature/deck/editor/card-table/card-table-menu.tsx
@@ -1,4 +1,6 @@
+import { DeleteCardConfirmDialog } from '@/components/feature/card'
 import { Button } from '@/components/ui/button'
+import { Dialog } from '@/components/ui/dialog'
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -6,26 +8,61 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
 import { EllipsisVertical } from 'lucide-react'
+import { useCallback, useState } from 'react'
+import { useDeleteCardOnCardTableMenuMutation } from './card-table-menu.generated'
+import { toast } from 'sonner'
+import { parseGQLError } from '@/lib/gql'
 
 type CardTableMenuProps = {
   cardId: number
 }
 
 export const CardTableMenu = ({ cardId }: CardTableMenuProps) => {
-  // TODO Delete dialog
+  const [openDelete, setOpenDelete] = useState(false)
+  const [{ fetching }, deleteCard] = useDeleteCardOnCardTableMenuMutation()
+
+  const handleSubmit = useCallback(async () => {
+    const { error } = await deleteCard(
+      {
+        input: {
+          id: cardId,
+        },
+      },
+      { additionalTypenames: ['Card'] },
+    )
+
+    if (error) {
+      const e = parseGQLError(error)
+      toast.error(e.message)
+    } else {
+      setOpenDelete(false)
+      toast.success('Card has been deleted')
+    }
+  }, [cardId, deleteCard])
+
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="ghost" size="icon">
-          <EllipsisVertical className="w-5 h-5" />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuItem>Edit</DropdownMenuItem>
-        <DropdownMenuItem className="text-destructive focus:text-destructive">
-          Delete
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <>
+      <DropdownMenu modal={false}>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" size="icon">
+            <EllipsisVertical className="w-5 h-5" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem>Edit</DropdownMenuItem>
+          <DropdownMenuItem
+            className="text-destructive focus:text-destructive"
+            onClick={() => {
+              return setOpenDelete(true)
+            }}
+          >
+            Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <Dialog open={openDelete} onOpenChange={setOpenDelete}>
+        <DeleteCardConfirmDialog loading={fetching} onSubmit={handleSubmit} />
+      </Dialog>
+    </>
   )
 }
