41dced9d30882641772312be3635f778ce1d6e52
RyoheiTomiyama <tennisnowboarderyou@gmail.com>
2024-11-22 23:12:11 +0900

    feat: ヘルプマーク表示

--- src/components/feature/setting/learning-option/learning-option-dropdown/learning-option-dropdown-content.tsx
+++ src/components/feature/setting/learning-option/learning-option-dropdown/learning-option-dropdown-content.tsx
@@ -2,13 +2,17 @@ import { DropdownMenuContent } from '@/components/ui/dropdown-menu'
 import React from 'react'
 import { learningOptions } from '../learning-options'
 import { Switch } from '@/components/ui/switch'
+import { CircleHelp } from 'lucide-react'
 
 export const LearningOptionDropdownContent = () => {
   return (
     <DropdownMenuContent>
       <label className="flex flex-row items-center gap-2 px-2 py-1.5 text-sm">
-        <Switch />
-        {learningOptions.voiceOnly.name}
+        <div className="flex flex-row items-center gap-2">
+          <Switch />
+          {learningOptions.voiceOnly.name}
+        </div>
+        <CircleHelp className="w-4 h-4" />
       </label>
       <label className="flex flex-row items-center gap-2 px-2 py-1.5 text-sm">
         <Switch />
